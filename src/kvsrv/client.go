0a2a1b2e524bd132da497bc91dd1efb51d27f62c
sakura <1927346336@qq.com>
2024-07-17 22:47:12 +0800

    finish kvserver.A(no network failures)

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,9 +1,11 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
@@ -35,9 +37,13 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+
+	args := &GetArgs{Key: key}
+	reply := &GetReply{}
+
+	ck.server.Call("KVServer.Get", args, reply)
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -50,7 +56,15 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
-	return ""
+
+	args := &PutAppendArgs{
+		Key:   key,
+		Value: value,
+	}
+	reply := &PutAppendReply{}
+
+	ck.server.Call("KVServer."+op, args, reply)
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,45 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	data map[string]string
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	reply.Value = kv.data[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.data[args.Key] = args.Value
 }
 
+// 将新的值追加到记录中，并返回旧的记录
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	old := kv.data[args.Key]
+	kv.data[args.Key] = old + args.Value
+	reply.Value = old
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.data = make(map[string]string)
 
 	return kv
 }
